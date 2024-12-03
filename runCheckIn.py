374dfafb60197893eaa182b6ce203a8cb65fe8a5
Calx Karst <57324710+CalxKarst@users.noreply.github.com>
2024-12-03 21:57:42 +0530

    Create runCheckIn.py

---
+++ runCheckIn.py
@@ -0,0 +1,20 @@
+import genshin as gs
+import asyncio, os
+
+async def claimDaily(client) :
+  try: 
+    reward = await client.claim_daily_reward()
+  except :
+    print("Daily reward already claimed")
+  else:
+    print(f"Claimed {reward.amount}x {reward.name}")
+  return
+
+def main():
+  ltuid = int(os.getenv("LT_UID"))
+  ltoken = os.getenv("LT_TOKEN")
+  client = gs.Client({"ltuid": ltuid, "ltoken": ltoken})
+  asyncio.run(claimDaily(client))
+
+if __name__ == "__main__":
+  main()
