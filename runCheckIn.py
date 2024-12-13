dc3e4c174abf99787a9fd8dd6f68ee060f42ab64
Calx Karst <57324710+CalxKarst@users.noreply.github.com>
2024-12-13 20:55:53 +0530

    Update runCheckIn.py

--- runCheckIn.py
+++ runCheckIn.py
@@ -11,19 +11,14 @@ async def claimDaily(client) :
     print(f"Claimed {reward.amount}x {reward.name}")
   return
 
-def main():
-  #ltuid = int(os.getenv("LT_UID"))
-  #ltoken = os.getenv("LT_TOKEN")
-  emailAddr = os.getenv("EMAIL_ADDR")
-  passWord = os.getenv("PASSWORD")
-  client = gs.Client()
-  cookies = dict(asyncio.run(client.login_with_password(emailAddr,passWord)))
-  cookies = gs.utility.get_browser_cookies()
-  #client.set_cookies(ltuid=cookies["ltuid_v2"],ltoken=cookies["ltoken_v2"])
-  print(cookies)
-  client.set_cookies(cookies)
-  client.default_game = gs.Game.GENSHIN
-  asyncio.run(claimDaily(client))
-
-if __name__ == "__main__":
-  main()
+#ltuid = int(os.getenv("LT_UID"))
+#ltoken = os.getenv("LT_TOKEN")
+emailAddr = os.getenv("EMAIL_ADDR")
+passWord = os.getenv("PASSWORD")
+client = gs.Client()
+cookies = dict(asyncio.run(client.login_with_password(emailAddr,passWord)))
+#client.set_cookies(ltuid=cookies["ltuid_v2"],ltoken=cookies["ltoken_v2"])
+print(cookies)
+client.set_cookies(cookies)
+client.default_game = gs.Game.GENSHIN
+asyncio.run(claimDaily(client))
