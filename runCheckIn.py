15f8656311e7d57785ae48bd8018ad5230b13b44
Calx Karst <57324710+CalxKarst@users.noreply.github.com>
2026-02-11 21:48:12 +0530

    Update runCheckIn.py
    Changed the way asyncio runs

--- runCheckIn.py
+++ runCheckIn.py
@@ -1,24 +1,27 @@
 import genshin as gs
-import asyncio, os
+import asyncio
+import os
 
-async def claimDaily(client) :
-  try: 
-    reward = await client.claim_daily_reward()
-  except Exception as e:
-    print("Daily reward already claimed")
-    print(e)
-  else:
-    print(f"Claimed {reward.amount}x {reward.name}")
-  return
+async def claimDaily(client):
+    try:
+        reward = await client.claim_daily_reward()
+    except Exception as e:
+        print("Daily reward already claimed")
+        print(e)
+    else:
+        print(f"Claimed {reward.amount}x {reward.name}")
 
-#ltuid = int(os.getenv("LT_UID"))
-#ltoken = os.getenv("LT_TOKEN")
-emailAddr = os.getenv("EMAIL_ADDR")
-passWord = os.getenv("PASSWORD")
-client = gs.Client()
-cookies = dict(asyncio.run(client.login_with_password(emailAddr,passWord)))
-#client.set_cookies(ltuid=cookies["ltuid_v2"],ltoken=cookies["ltoken_v2"])
-print(cookies)
-client.set_cookies(cookies)
-client.default_game = gs.Game.GENSHIN
-asyncio.run(claimDaily(client))
+async def main():
+    emailAddr = os.getenv("EMAIL_ADDR")
+    passWord = os.getenv("PASSWORD")
+
+    client = gs.Client()
+    cookies = await client.login_with_password(emailAddr, passWord)
+
+    client.set_cookies(dict(cookies))
+    client.default_game = gs.Game.GENSHIN
+
+    await claimDaily(client)
+
+if __name__ == "__main__":
+    asyncio.run(main())
